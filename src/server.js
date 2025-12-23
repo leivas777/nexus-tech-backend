4a162aad5d0584e093c4dc65e09730e6be3a0406
leivas777 <145990683+leivas777@users.noreply.github.com>
2025-12-23 16:44:39 -0300

    Deploy to main branch

--- src/server.js
+++ src/server.js
@@ -62,7 +62,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ RESPONDER EXPLICITAMENTE A OPTIONS (importante!)
-app.options('*', cors(corsOptions));
+app.options((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.get('origin'));
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // ✅ Middleware de Log
 app.use((req, res, next) => {
